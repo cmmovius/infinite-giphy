c43af0cf38afa9107045269c1dda7d3a07e245f0
Christine Movius <christine.movius@gmail.com>
2016-03-30 09:35:34 -0400

    Search result yields first gif [0] image. woot woot.

---
+++ js/giphy.js
@@ -0,0 +1,38 @@
+// initial API call
+$(document).ready( function() {
+  $(".search").click(function(event){
+    event.preventDefault();
+    apiCall();
+  })
+});
+
+
+var apiCall = function() {
+  console.log('clicked');
+  // add code here to make the AJAX request!
+  var keyword = $("input[name='keyword']").val();
+  console.log(keyword);
+  var url = "http://api.giphy.com/v1/gifs/search?q="+keyword+"&api_key=dc6zaTOxFJmzC";
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: "json"
+  }).done ( function(response){
+    console.log(response);
+    // call giphy function below to append gif's
+    giphy(response);
+  }).fail ( function (){
+    console.log("fail");
+  }).always( function(){
+    console.log("Something happens");
+  })
+
+}
+
+
+var giphy = function(response) {
+  // add code here to append data to the body!
+  $('body').append("<div class = giphy-url></div>")
+  // $('.giphy-url').append("<p>"+response.data[0].url+"</p>")
+  $('.giphy-url').append("<img src = '"+response.data[0].images.fixed_height_small.url+"'>")
+}
